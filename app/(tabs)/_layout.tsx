8b3f3eb372cc66c7b1c2946ca4ca108ef43d7fed
Sarthak Kimtani <sarthakkimtani123@gmail.com>
2025-12-22 22:48:02 +0530

    feat: add native tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,54 @@
-import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Icon, Label, NativeTabs, VectorIcon } from "expo-router/unstable-native-tabs";
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+export default function TabLayout() {
+  return (
+    <NativeTabs
+      minimizeBehavior="onScrollDown"
+      labelVisibilityMode="labeled"
+      indicatorColor="#00000021"
+      backgroundColor="#F9FAFB"
+    >
+      <NativeTabs.Trigger name="home">
+        <Label>Home</Label>
+        <Icon
+          selectedColor={"#000"}
+          src={{
+            default: <VectorIcon family={Ionicons} name="home-outline" />,
+            selected: <VectorIcon family={Ionicons} name="home" />,
+          }}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="stocks">
+        <Label>Stocks</Label>
+        <Icon
+          selectedColor={"#000"}
+          src={{
+            default: <VectorIcon family={Ionicons} name="trending-up-outline" />,
+            selected: <VectorIcon family={Ionicons} name="trending-up" />,
+          }}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="watchlist">
+        <Label>Watchlist</Label>
+        <Icon
+          selectedColor={"#000"}
+          src={{
+            default: <VectorIcon family={Ionicons} name="eye-outline" />,
+            selected: <VectorIcon family={Ionicons} name="eye" />,
+          }}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="members">
+        <Label>Members</Label>
+        <Icon
+          selectedColor={"#000"}
+          src={{
+            default: <VectorIcon family={Ionicons} name="people-outline" />,
+            selected: <VectorIcon family={Ionicons} name="people" />,
+          }}
+        />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
---
+++ app/(tabs)/stocks.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function StocksScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Stocks</Text>
+    </View>
+  );
+}
